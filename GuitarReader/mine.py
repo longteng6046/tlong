9d2fc52ad42eb82fe763974ed29f3528a4f6cd36
tlongcn@gmail.com <tlongcn@gmail.com@e8f1d18a-1aba-0eb4-51dc-60335f9c19a4>
2011-06-05 20:30:32 +0000

    Private functions.

---
+++ GuitarReader/mine.py
@@ -0,0 +1,13 @@
+##################################################
+#
+# File: mine.py
+# Description:
+#   my tool functions.
+#
+##################################################
+
+## function: printl(list)
+##   list can be [], {}, as long as it can be represented as "for item in list"
+def printl(list):
+    for item in list:
+        print item
