4b00db0171229477b22c616be4d482a2f8fc8bed
tlongcn <tlongcn@gmail.com>
2011-06-20 02:44:14 +0000

    formalize xml files from one directory and save them in the other directory.

---
+++ GuitarReader/formalize.py
@@ -0,0 +1,52 @@
+##################################################
+#
+# File: formalize.py
+# Description:
+#   Formalize the xml files to be in the sorted order
+#   
+#
+##################################################
+
+import sys
+import os
+import codecs
+
+# from staCompHelper import *
+from myxml import *
+
+
+
+if len(sys.argv) != 3:
+    print >> sys.stderr, "Error."
+    print >> sys.stderr, "\tFormat: python ./formalize.py DIR/TO/xml/FILES DIR/TO/SAVE"
+    sys.exit(1)
+
+
+dir1 = sys.argv[1]
+dir2 = sys.argv[2]
+
+testList = os.listdir(dir1)
+
+counter = 0
+
+for item in testList:
+    counter += 1
+    xmlfile = codecs.open(dir1 + '/' + item, encoding='utf-8')
+
+
+    if len(xmlfile.readlines()) < 10:
+        continue
+
+    destfile = codecs.open(dir2 + '/' + item, encoding='utf-8', mode='w')
+    xmlfile.seek(0)
+    
+    xtree = Xtree()
+    xtree.parseFile(xmlfile)
+    xtree.formalize()
+    writeXML(xtree.root, destfile)
+
+    xmlfile.close()
+    destfile.close()
+
+    if counter - counter / 10 * 10 == 0:
+        print counter
