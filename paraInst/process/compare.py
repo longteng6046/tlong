b84cb8bb82af19e994f6af010603ad4e5f14e491
tlongcn@gmail.com <tlongcn@gmail.com@e8f1d18a-1aba-0eb4-51dc-60335f9c19a4>
2011-11-30 05:01:13 +0000

    add processing code of collected data

---
+++ paraInst/process/compare.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+
+list = ['self.txt', 'freepooma.txt', 'parmetis.txt', 'petsc.txt']
+
+funDir = {}
+
+funDir = {}
+
+for file in list:
+
+    fd = open(file)
+    lines = fd.readlines()
+
+    for line in lines:
+        if ':' not in line:
+            continue
+
+        funName = line.split(':')[0].strip()
+        scale = line.split(':')[1].strip()
+
+        if funName not in funDir:
+            funDir[funName] = {}
+            funDir[funName][file] = scale
+        else:
+            funDir[funName][file] = scale
+    
+
+resultDir = {}
+
+
+ctr = 0
+for funName in funDir:
+    # print item, funDir[item]
+
+    subDir = funDir[funName]
+
+    if "self.txt" not in subDir:
+        # print subDir
+        continue
+    
+
+    # (low_bound, high_bound) = subDir["self.txt"]
+
+    low_bound = subDir["self.txt"].strip()[1:-1].split(',')[0]
+    if '.' in low_bound:
+        low_bound = float(low_bound)
+    else:
+        low_bound = int(low_bound)
+    high_bound = subDir["self.txt"].strip()[1:-1].split(',')[1]
+
+    if '.' in high_bound:
+        high_bound = float(high_bound)
+    else:
+        high_bound = int(high_bound)
+    # print low_bound, high_bound
+
+    flag = False
+
+    for item in subDir:
+        if item != "self.txt":
+            low = subDir[item].strip()[1:-1].split(',')[0]
+            if '.' in low:
+                low = float(low)
+            else:
+                low = int (low)
+                
+            high = subDir[item].strip()[1:-1].split(',')[1]
+            if '.' in high:
+                high = float(high)
+            else:
+                high = int(high)
+            # print low, high
+            if low < low_bound or high > high_bound:
+                flag = True
+                print item, "(", low, ",", high, ")"
+    if flag == True:
+        print "self: (", low_bound, ",", high_bound, ")"
+        print funName
+        ctr += 1
+        print ctr
+        print "--------------------------------------------------------------------------------"
---
+++ paraInst/process/process.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+if len(sys.argv) != 3:
+    print >> sys.stderr, "./" + sys.argv[0] + " source_dir dest_dir"
+    sys.exit(1)
+
+srcdir = sys.argv[1]
+desdir = sys.argv[2]
+
+fileList = os.listdir(srcdir)
+# print fileList
+
+fd_des = open(desdir, 'w')
+
+for infile in fileList:
+
+    # print infile
+    values = open(srcdir + infile).xreadlines()
+
+    # print >> fd_des, infile[:-4]
+
+    fd_des.write(infile[:-4])
+    fd_des.write(" : ")
+    
+    valSet = set([])
+    for item in values:
+        item = item.strip()
+        # print item
+        if '.' in item:
+            valSet.add(float(item))
+        else:
+            valSet.add(int(item))
+
+
+    # print >> fd_des, '(', min(valSet), ',', max(valSet), ')'
+
+    scale = '(' + str(min(valSet)) + ',' + str(max(valSet)) + ')' + '\n'
+    
+    fd_des.write(scale)
+    
+    print >> fd_des, len(valSet)
+    # for item in valSet:
+        # print >> fd_des, item
+
+    print >> fd_des, ""
